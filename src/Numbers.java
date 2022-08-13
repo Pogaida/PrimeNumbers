a1b27d7a5c51ba475edca4338a1ffb42677ac8e4
Id:Projekt <g20dawid@gmail.com>
2022-08-13 14:12:30 +0200

    version 1

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,2 +1,19 @@
 public class Numbers {
+    public static void main(String[] args) {
+        //generujemy liczbę
+        for (int number = 2; number <= 1_000; number++) {
+            //podajemy dzielnik na 1 ponieważ (liczba % 1 = liczba) zawsze jest prawdą
+            int divider = 1;
+            for (int i = number; i >= 2; i--) {
+                //sprawdzmy czy liczba ma więcej niż jeden dzielnik
+                if(number % i == 0) {
+                    divider++;
+                }
+            }
+            //jeśli liczba ma tylko dwa dzielniki (numer oraz 1) to jest to liczba pierwsza
+            if(divider == 2){
+                System.out.println("[" + number + "]");
+            }
+        }
+    }
 }
