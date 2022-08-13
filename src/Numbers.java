b8f23d596e64d587641c6d8f75894e38b1aa191c
Id:Projekt <g20dawid@gmail.com>
2022-08-13 22:27:01 +0200

    version 2.2

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,17 +1,21 @@
 public class Numbers {
     public static void main(String[] args) {
         //generujemy liczbę
-        for (int number = 2; number <= 5_000; number++) {
+        long alfaTime = System.currentTimeMillis();
+        for (int number = 2; number <= 100_000; number++) {
             int divider = 0;
             for (int i = 1; i <= number; i++) {
                 //sprawdzmy ile liczba ma dzielników
                 if(number % i == 0) divider++;
 
                 //jeśli ma więcej niż 2 dzielniki przerywa pętlę (przyspiesza program)
-                if(divider > 2) break;
+                if(divider > 2 && i < number) break;
             }
             //jeśli liczba ma 2 dzielniki wypisujemy liczbę
             if(divider == 2) System.out.println("[" + number + "]");
         }
+        long omegaTime = System.currentTimeMillis() - alfaTime;
+        //czas działania programu w sekundach
+        System.out.println("Time: " + omegaTime / 1000 + "sek");
     }
 }
