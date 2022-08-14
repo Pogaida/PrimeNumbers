c6e0e7d99f04b7f77f832e76b4b9bef29a1bc055
Id:Projekt <g20dawid@gmail.com>
2022-08-14 13:18:24 +0200

    version 2.5

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,21 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Numbers {
     public static void main(String[] args) {
-        //generujemy liczbę
+        ArrayList<Integer> zapis = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         long alfaTime = System.currentTimeMillis();
-        for (int number = 2; number <= 100_000; number++) {
+
+        //generujemy liczbę
+        for (int number = 2; number <= 10_000; number++) {
             int divider = 0;
             for (int i = 1; i <= number; i++) {
+
                 //sprawdzmy ile liczba ma dzielników
                 if(number % i == 0) divider++;
 
                 //jeśli ma więcej niż 2 dzielniki przerywa pętlę (przyspiesza program)
-                if(divider > 2 && i < number) break;
+                if(divider > 2) break;
             }
-            //jeśli liczba ma 2 dzielniki wypisujemy liczbę
-            if(divider == 2) System.out.println("[" + number + "]");
+
+            //jeśli liczba ma 2 dzielniki wpisujemy liczbę do listy
+            if(divider == 2){
+                zapis.add(number);
+            }
+
         }
         long omegaTime = System.currentTimeMillis() - alfaTime;
+
         //czas działania programu w sekundach
         System.out.println("Time: " + omegaTime / 1000 + "sek");
+
+        //wpisujemy indeks listy i wyświelta nam zapisaną liczbę
+        podajIndeks(zapis, scanner);
+    }
+
+    private static void podajIndeks(ArrayList<Integer> zapis, Scanner scanner) {
+        System.out.println("Podaj indeks z listy 1-" + zapis.size());
+        for (int i = 0; i <= 10; i++) {
+            System.out.print("I:");
+            try {
+                int indeks = scanner.nextInt();
+                System.out.println(zapis.get(indeks-1));
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("Poza granicą");
+            }
+        }
     }
 }
