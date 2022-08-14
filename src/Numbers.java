a396aca0998cc0381b46f73717de210d2eca0734
Id:Projekt <g20dawid@gmail.com>
2022-08-14 16:51:01 +0200

    version 3 refactor

--- src/Numbers.java
+++ src/Numbers.java
@@ -6,32 +6,26 @@ public class Numbers {
         ArrayList<Integer> zapis = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         long alfaTime = System.currentTimeMillis();
-
-        //generujemy liczbę
-        for (int number = 2; number <= 10_000; number++) {
+        for (int number = 2; number <= 100_000; number++) {
             int divider = 0;
-            for (int i = 1; i <= number; i++) {
-
-                //sprawdzmy ile liczba ma dzielników
-                if(number % i == 0) divider++;
-
-                //jeśli ma więcej niż 2 dzielniki przerywa pętlę (przyspiesza program)
-                if(divider > 2) break;
-            }
-
-            //jeśli liczba ma 2 dzielniki wpisujemy liczbę do listy
-            if(divider == 2){
-                zapis.add(number);
-            }
-
+            divider = sprawdzIleDzielnikow(number, divider);
+            if(divider == 2) zapis.add(number);
         }
         long omegaTime = System.currentTimeMillis() - alfaTime;
-
-        //czas działania programu w sekundach
         System.out.println("Time: " + omegaTime / 1000 + "sek");
-
-        //wpisujemy indeks listy i wyświelta nam zapisaną liczbę
         podajIndeks(zapis, scanner);
+    }
+
+    private static int sprawdzIleDzielnikow(int number, int divider) {
+        for (int i = 1; i <= number; i++) {
+
+            //sprawdzmy ile liczba ma dzielników
+            if(number % i == 0) divider++;
+
+            //jeśli ma więcej niż 2 dzielniki przerywa pętlę (przyspiesza program)
+            if(divider > 2) break;
+        }
+        return divider;
     }
 
     private static void podajIndeks(ArrayList<Integer> zapis, Scanner scanner) {
