2b0d991807c56b5b2d6dfe07c98f051b19625a15
Id:Projekt <g20dawid@gmail.com>
2022-08-14 20:20:37 +0200

    version 3.5 Mega refactor

--- src/Numbers.java
+++ src/Numbers.java
@@ -7,35 +7,33 @@ public class Numbers {
         Scanner scanner = new Scanner(System.in);
         long alfaTime = System.currentTimeMillis();
         for (int number = 2; number <= 100_000; number++) {
-            int divider = 0;
-            divider = sprawdzIleDzielnikow(number, divider);
-            if(divider == 2) zapis.add(number);
+            ileDzielnikow(zapis, number);
         }
         long omegaTime = System.currentTimeMillis() - alfaTime;
         System.out.println("Time: " + omegaTime / 1000 + "sek");
         podajIndeks(zapis, scanner);
     }
 
-    private static int sprawdzIleDzielnikow(int number, int divider) {
+    private static void ileDzielnikow(ArrayList<Integer> zapis, int number) {
+        int divider = 0;
         for (int i = 1; i <= number; i++) {
-
-            //sprawdzmy ile liczba ma dzielników
             if(number % i == 0) divider++;
-
-            //jeśli ma więcej niż 2 dzielniki przerywa pętlę (przyspiesza program)
-            if(divider > 2) break;
+            if(divider == 2 && i < number) {
+                divider++;
+                break;
+            }
         }
-        return divider;
+        if(divider == 2)zapis.add(number);
     }
 
     private static void podajIndeks(ArrayList<Integer> zapis, Scanner scanner) {
         System.out.println("Podaj indeks z listy 1-" + zapis.size());
         for (int i = 0; i <= 10; i++) {
-            System.out.print("I:");
+            System.out.print("Id:");
             try {
                 int indeks = scanner.nextInt();
                 System.out.println(zapis.get(indeks-1));
-            }catch (IndexOutOfBoundsException e){
+            } catch (IndexOutOfBoundsException e){
                 System.out.println("Poza granicą");
             }
         }
