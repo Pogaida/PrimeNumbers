f81fb2503f3c71eaf7479e9a578098a11bebde73
kowallus <kowallus@o2.pl>
2022-11-02 14:24:50 +0100

    drobne porządki, np. wyciąganie stałych i komunikacja

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
 
 public class Numbers {
-    public static void main(String[] args) {
-        PrimeNumberRangeAnalyzer check = new PrimeNumberRangeAnalyzer(1, 10_000_000);
-        Scanner scanner = new Scanner(System.in);
 
-        //Wpisujemy liczbę i sprawdzamy czy jest na liście
-        System.out.println("1.Liczba");
-        System.out.println("2.Komurka od 1 do " + check.getSizeOfList());
-        while (true){
-            System.out.print("\nWybór:");
-            switch (scanner.nextInt()){
-                case 1:
-                    System.out.print("Podaj:");
-                    int number = scanner.nextInt();
-                    boolean isOnList = check.isNumberOfList(number);
-                    if(isOnList){
-                        System.out.println("TRUE");
-                    }
-                    else {
-                        System.out.println("FALSE");
-                    }
-                    break;
-                case 2:
-                    System.out.print("Podaj:");
-                    int cell = scanner.nextInt();
-                    try {
-                        System.out.println(check.getRecordOfList(cell-1));
-                    }catch (IndexOutOfBoundsException e){
-                        System.out.println("Out of bounds");
-                    }
+	private static final int PRIME_ANALYZER_RANGE_END = 10_000_000;
 
-            }
-        }
-    }
+	public static void main(String[] args) {
+		PrimeNumberRangeAnalyzer check = new PrimeNumberRangeAnalyzer(1, PRIME_ANALYZER_RANGE_END);
+		Scanner scanner = new Scanner(System.in);
+
+		// Wpisujemy liczbę i sprawdzamy czy jest na liście
+		System.out.println("1. Test liczby");
+		System.out.println("2. Kolejna liczba pierwsza (od 1 do " + check.getSizeOfList() + ")");
+		while (true) {
+			System.out.print("\nTwój wybór: ");
+			switch (scanner.nextInt()) {
+			case 1:
+				System.out.print("Podaj liczbę: ");
+				int number = scanner.nextInt();
+				boolean isOnList = check.isNumberOfList(number);
+				if (isOnList) {
+					System.out.println("To jest liczba pierwsza.");
+				} else {
+					System.out.println("To nie jest liczba pierwsza.");
+				}
+				break;
+			case 2:
+				System.out.print("Podaj numer liczby pierwszej: ");
+				int cell = scanner.nextInt();
+				try {
+					System.out.println(check.getRecordOfList(cell - 1));
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println("Przekroczony zakres!");
+				}
+				break;
+			default:
+				System.out.print("Nieobsługiwana opcja!");
+			}
+		}
+	}
 }
